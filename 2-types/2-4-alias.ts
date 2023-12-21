24d22368cabea6b6569a19b2d6e1272175f90360
DanielSon93 <ths_eksldpf@naver.com>
2023-12-21 15:20:17 +0900

    2-4-alias

---
+++ 2-types/2-4-alias.ts
@@ -0,0 +1,26 @@
+{
+  // Type Aliases : 새로운 타입 생성
+  type Str = string;
+  type Num = number;
+  let typeStr: Str = '안녕하세요';
+  let typeNum: Num = 5;
+
+  type Student = {
+    name: string,
+    age: number,
+  }
+
+  const student: Student = {
+    name: 'Daniel',
+    age: 200,
+  }
+  console.log(student);
+
+  // Literal Types : 특정 데이터로 명시 가능
+  type Name = 'Daniel';
+  type Age = 10;
+  type Human = true;
+  const name: Name = 'Daniel';
+  const age: Age = 10;
+  const isHuman: Human = true;
+}
